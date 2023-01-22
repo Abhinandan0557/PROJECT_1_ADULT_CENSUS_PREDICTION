135531fd5daf8647b5d449f3b0583b9326bc1086
Abhinandan0557 <abhi.c0557@gmail.com>
2023-01-22 19:12:40 +0530

    requirement list created in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,26 @@
+
+'''this file looks for all requirements and dependencies and install them.
+find_packages() - is used to find which folder conatin __init__.py file and consider these folder as packages
+'''
+
 from setuptools import find_packages,setup
+
+from typing import List
+
+REQUIREMENT_FILE_NAME="requirements.txt"
+HYPHEN_E_DOT = "-e ."  #this -e . is required in requirements.txt to trigger setupe.py file at start.
+
+#creating list of requirement libraries
+def get_requirements()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_list]
+
+#-e . is not a library so we have to remove it
+if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
+
 
 setup(
     name="ADULT_CEBNSUS_PREDICTION",
